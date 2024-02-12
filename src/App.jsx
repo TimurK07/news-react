821f0dc41e41cfae06ef0a3b5a056eb5f36ce768
Timur Kaliyev <timurkaliev115@gmail.com>
2024-02-12 20:52:02 +0600

    git init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header/Header';
+import Main from './pages/Main/Main';
+
+function App() {
+  return (
+  <>
+    <Header/>
+    <div className='container'>
+      <Main/>
+    </div>
+    
+    
+  </>
+  );
+}
+
+export default App;
