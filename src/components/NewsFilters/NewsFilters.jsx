22c4948666ea48d91cd442360f391599691ce82b
Timur Kaliyev <timurkaliev115@gmail.com>
2024-02-19 14:03:30 +0600

    fix slider and banners scroll

--- src/components/NewsFilters/NewsFilters.jsx
+++ src/components/NewsFilters/NewsFilters.jsx
@@ -1,19 +1,24 @@
-import { getCategories } from '../../api/apiNews';
-import { useFetch } from '../../helpers/hooks/useFetch';
-import Categories from '../Categories/Categories';
-import Search from '../Search/Search';
+import { getCategories } from "../../api/apiNews";
+import { useFetch } from "../../helpers/hooks/useFetch";
+import Categories from "../Categories/Categories";
+import Search from "../Search/Search";
+import Slider from "../Slider/Slider";
 import styles from "./styles.module.css";
 
-const NewsFilters = ({filters, changeFilter}) => {
+const NewsFilters = ({ filters, changeFilter }) => {
   const { data: dataCategories } = useFetch(getCategories);
   return (
     <div className={styles.filters}>
       {dataCategories ? (
-        <Categories
-          categories={dataCategories.categories}
-          selectedCategory={filters.category}
-          setSelectedCategory={(category) => changeFilter("category", category)}
-        />
+        <Slider>
+          <Categories
+            categories={dataCategories.categories}
+            selectedCategory={filters.category}
+            setSelectedCategory={(category) =>
+              changeFilter("category", category)
+            }
+          />
+        </Slider>
       ) : null}
 
       <Search
