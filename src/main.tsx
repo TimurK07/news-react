c242393a8bf9ab1b67fcdcc7144938f9f4e54132
TimurK07 <webprogramming07t@gmail.com>
2024-05-14 17:36:57 +0500

    add rtk query and redux toolkot

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from './context/ThemeContext.tsx';
+import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { store } from "./store/index.ts";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
